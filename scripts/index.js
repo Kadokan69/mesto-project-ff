439a72977d533aed8e7772c95aba85313b743d5f
Kelorev Andrey <kelorev.andrey@yandex.ru>
2024-07-05 12:32:15 +0300

    Функционал реализован, карточки добавляются и удаляются 05.07.2024 12:32

--- scripts/index.js
+++ scripts/index.js
@@ -9,37 +9,22 @@
 // @todo: Вывести карточки на страницу
 
 const cardTemplate = document.querySelector('#card-template').content;
-const cardElement = cardTemplate.querySelector('.card');
-const cardTitle = cardElement.querySelector('.card__title');
-const cardImage = cardElement.querySelector('.card__image');
 const placesList = document.querySelector('.places__list');
 
-
-
-
-
 function createCard(card) {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardElement = cardTemplate.cloneNode(true);
     const removeButton = cardElement.querySelector('.card__delete-button');
   
     cardElement.querySelector('.card__title').textContent = card.name;
-    cardElement.querySelector('.card__image').setAttribute('src', card.link);
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = card.name;
     
-    removeButton.addEventListener('click', deleteCard);
-    
-    placesList.append(cardElement);
-    console.log(cardElement);
+    removeButton.addEventListener('click', function() {
+        const deleteElementCard = removeButton.closest('.card');
+            deleteElementCard.remove();
+        })
 
-    return cardElement;
-  };
-
-
-
-function deleteCard(){
-    // const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-        cardElement.remove();
-        console.log("cardElement");
-    };
-    
+    placesList.append(cardElement); 
+  }
 
 initialCards.forEach(createCard);
