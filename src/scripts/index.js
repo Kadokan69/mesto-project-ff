35c32c705954ec3481950701309e4cd596fb17bd
Andrey Kelorev <kelorev.andrey@yandex.ru>
2024-08-06 10:07:49 +0300

    Реализация Редактирования профиля и Добавления новых карточек

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,7 @@ import {openPopup} from '../scripts/popup.js'
 const cardTemplate = document.querySelector('#card-template').content;
 const placesContainer = document.querySelector('.places__list');
 
+
 function createCard(card) {
     const cardElement = cardTemplate.cloneNode(true);
     const removeButton = cardElement.querySelector('.card__delete-button');
@@ -41,3 +42,35 @@ const popupNewCard = document.querySelector('.popup_type_new-card');
 
 addCardButton.addEventListener('click', () => openPopup(popupNewCard));
 
+//Редактирование Профиля
+const formElement = document.querySelector('.popup_type_edit');// Воспользуйтесь методом querySelector()
+const nameInput = document.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
+const jobInput = document.querySelector('.popup__input_type_description');// Воспользуйтесь инструментом .querySelector()
+
+function handleFormSubmit(evt) {
+    evt.preventDefault(); 
+      document.querySelector('.profile__title').textContent = nameInput.value;
+      document.querySelector('.profile__description').textContent = jobInput.value
+      nameInput.value = '';
+      jobInput.value = '';
+    popupEdit.classList.remove('popup_is-opened');
+    popupEdit.classList.add('popup_is-animated');
+
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+const newCardName = document.querySelector('.popup__input_type_card-name');
+const newCardUrl = document.querySelector('.popup__input_type_url');
+
+function newCardForem(evt) {
+  evt.preventDefault(); 
+  let newCard = {name:newCardName.value, link:newCardUrl.value};
+  placesContainer.prepend(createCard(newCard))
+  popupNewCard.classList.remove('popup_is-opened');
+  popupNewCard.classList.add('popup_is-animated');
+  newCardName.value = '';
+  newCardUrl.value = '';
+};
+
+popupNewCard.addEventListener('submit', newCardForem);
