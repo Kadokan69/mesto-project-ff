45520f5d135dcb062cff38e56e637d86f63de777
Andrey Kelorev <kelorev.andrey@yandex.ru>
2024-08-23 12:29:17 +0300

    Ревлизация добавления лайков и их отображение в карточках через Api

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -7,19 +7,24 @@ export function createCard(card, cardTemplate, likeCard, addContentCardPopup, op
   const likeScore = cardElement.querySelector(".card__like-score");
   const cardTitile = cardElement.querySelector(".card__title");
   const deleteCardPopup = document.querySelector(".popup_type_delete-card");
-  if(card.owner._id !== '8e978d0da67fdc629650fdb2'){
-    removeButton.remove();
-  };
+  
   cardTitile.textContent = card.name;
   cardImage.src = card.link;
   cardImage.alt = card.name;
-  likeScore.textContent = card.likes.length
+  likeScore.textContent = card.likes.length;
   cardImage.addEventListener("click", () =>
     addContentCardPopup(cardImage.src, cardTitile.textContent),
   );
   removeButton.addEventListener("click", () => deleteCard(removeButton, deleteCardPopup, openPopup, closePopup, card));
-  likeButton.addEventListener("click", () => likeCard(likeButton));
-  
+  likeButton.addEventListener("click", () => likeCard(likeButton, card, likeScore));
+  if(card.owner._id !== '8e978d0da67fdc629650fdb2'){
+    removeButton.remove();
+  };
+  card.likes.forEach(element => {
+    if(element._id === '8e978d0da67fdc629650fdb2'){
+      likeButton.classList.toggle("card__like-button_is-active")
+    };
+  });
 
   return cardElement;
 }
@@ -46,7 +51,42 @@ function deleteCard(deleteItem, deleteCardPopup, openPopup, closePopup, card) {
 
 
 //Лайк карточки
-export function likeCard(likeButton) {
+export function likeCard(likeButton, card, likeScore) {
   likeButton.classList.toggle("card__like-button_is-active")
+  if(likeButton.classList.contains("card__like-button_is-active")){
+    fetch(`https://nomoreparties.co/v1/wff-cohort-21/cards/likes/${card._id} `, {
+      method: 'PUT',
+      headers: {
+       authorization: 'a8ed8923-c1b8-4d93-ac19-168eae7fcf29',
+     }
+     })
+     .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    })
+     .then((result) => {
+      likeScore.textContent = result.likes.length;
+     });
+     
+  } else{
+    fetch(`https://nomoreparties.co/v1/wff-cohort-21/cards/likes/${card._id} `, {
+      method: 'DELETE',
+      headers: {
+       authorization: 'a8ed8923-c1b8-4d93-ac19-168eae7fcf29',
+     }
+     })
+     .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    })
+     .then((result) => {
+      likeScore.textContent = result.likes.length;
+     });
+    
+  };
 }
 
